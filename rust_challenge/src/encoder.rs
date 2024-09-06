b8d0b0938163490da34f23721456d5a998dc0f80
SolsticeSpectrum <38611351+SolsticeSpectrum@users.noreply.github.com>
2024-09-06 23:38:26 +0200

    Update encoder.rs

--- rust_challenge/src/encoder.rs
+++ rust_challenge/src/encoder.rs
@@ -5,7 +5,7 @@ pub struct Encoder;
 impl Encoder {
     pub fn encode(image: &DynamicImage) -> DynamicImage {
         // TODO: Implement the encoding logic
-		// Note: This is just a mockup implementation
+	// Note: This is just a mockup implementation
 		
         image.clone()
     }
